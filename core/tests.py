1e3d69ea63659c48b135557e8234268ee52dcf1e
usopp2324 <zakihhh727@gmail.com>
2026-09-01 18:19:48 +0100

    second

--- core/tests.py
+++ core/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
 
-# Create your tests here.
+
+class InternationalizationTests(TestCase):
+    def test_english_locale_route_loads_homepage(self):
+        response = self.client.get('/en/')
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'Home')
+        self.assertContains(response, 'Contact us')
+
+    def test_about_page_uses_translated_navigation(self):
+        response = self.client.get('/en/about/')
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'About')
+        self.assertContains(response, 'Contact')
